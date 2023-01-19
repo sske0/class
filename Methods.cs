087c5dee061cf7e2c79c615c4add49a2d7b65c70
Akbar A. Zulfugarov <akbaraz@code.local>
2023-01-19 21:45:04 +0400

    added

---
+++ Methods.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace testtttt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr1 = { 1, 34, 5, 6 };
+            //int sum = Findsum(arr1);
+            //Console.WriteLine(sum);
+
+
+            //welcomeMessage("Name", "Surname");
+
+
+            //reversedWord("bruh");
+
+
+            //FindNumOfSpaces("hello world and some spaces   ");
+
+
+
+        }
+
+        //static int FindArea(int n)
+        //{
+
+        //    return n*n;
+        //}
+        //static int FindArea(int length, int width)
+        //{
+        //    return length * width;
+        //}
+
+        //static int Findsum(int[] arr)
+        //{
+        //    int sum = 0;
+        //    foreach (int arrItem in arr)
+        //    {
+        //        sum += arrItem;
+        //    }
+        //    return sum;
+        //}
+
+        //static void welcomeMessage(string name, string surname)
+        //{
+        //    Console.WriteLine($"Welcome back, {name} {surname}!");
+        //}
+
+        //static void reversedWord(string word)
+        //{
+        //   string reversedString = "";
+            
+
+        //    for(int i = word.Length - 1; i >= 0; i--)
+        //    {
+
+        //            reversedString += word[i];
+ 
+        //    }
+        //    Console.WriteLine(reversedString);
+
+            
+        //}
+
+
+        //static void FindNumOfSpaces(string sentence)
+        //{
+        //    int count = 0;
+            
+        //    foreach(char word in sentence)
+        //    {
+        //        if (word == ' ')
+        //        {
+        //            count++;
+        //        }
+
+        //    }
+            
+               
+          
+        //    Console.WriteLine(count);
+            
+
+            
+        }
+    }
+}
